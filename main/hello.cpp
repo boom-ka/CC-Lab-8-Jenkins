7706403db9630a36400ae8c705ca1da5b7ab7ce5
boom-ka <83756504+boom-ka@users.noreply.github.com>
2025-03-13 01:32:52 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+
+    cout << "i am having so much fun !!! wohooo !!!\n";
   return 0;
 }
